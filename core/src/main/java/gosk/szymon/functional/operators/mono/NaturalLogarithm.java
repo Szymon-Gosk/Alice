13d5d23aab548f4321cb627b9420e8971d1e72f2
Szymon-Gosk <szymongosk767@gmail.com>
2021-09-29 00:19:24 +0200

    alice-0.3/DEV1: Add more operators in basic form

---
+++ core/src/main/java/gosk/szymon/functional/operators/mono/NaturalLogarithm.java
@@ -0,0 +1,35 @@
+package gosk.szymon.functional.operators.mono;
+
+import gosk.szymon.functional.Temporary;
+import gosk.szymon.functional.WorkInProgress;
+import gosk.szymon.functional.operators.Operator;
+import org.jetbrains.annotations.NotNull;
+
+public class NaturalLogarithm implements Operator {
+
+    @WorkInProgress
+    @Override
+    public Number evaluate(Number number) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "NaturalLogarithm{}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        return o != null && getClass() == o.getClass();
+    }
+
+    @Temporary
+    @Override
+    public @NotNull String debugString() {
+        return "LN";
+    }
+
+
+
+}
